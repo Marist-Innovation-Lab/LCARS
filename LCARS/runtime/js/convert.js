eeec0ab642a899b123295ef7fdb75a2d83c95565
Graham Burek <graham.burek1@gmail.com>
2016-10-03 10:30:23 -0400

    Changes fields for log data translator (source and destination IP addresses and ports).

--- LCARS/runtime/js/convert.js
+++ LCARS/runtime/js/convert.js
@@ -5,28 +5,41 @@ function convertToGstar() {
 	var data = logData.value;
 	var line = data.split("\n");
 	var jsonLine;
-	var ipColor = "black"
-	var countryColor = "green"
+	var srcIpColor = "red";
+	var destIpColor = "blue";
+	var srcPortColor = "yellow";
+	var destPortColor = "green";
+
 	output.innerHTML = "";
 	output.innerHTML = output.innerHTML + "new graph\n";
 
 	for (var i = 0; i < line.length; i++){
 		// Will need to refactor with regex to make dynamic.
 		jsonLine = JSON.parse(line[i]);
-		if(typeof jsonLine.ip == 'undefined' || typeof jsonLine.country == 'undefined'){
-			output.innerHTML = output.innerHTML + "ip or country is undefined in: " + JSON.stringify(jsonLine) + "\n";
+		if(typeof jsonLine.src == 'undefined' ||
+		   typeof jsonLine.src_port == 'undefined' ||
+		   typeof jsonLine.dest == 'undefined' ||
+		   typeof jsonLine.dest_port == 'undefined'){
+			output.innerHTML = output.innerHTML + "One or more of src, src_port, dest, dest_port are undefined in: " + JSON.stringify(jsonLine) + "\n";
 		} else {
-			output.innerHTML = output.innerHTML + "add vertex ip" + jsonLine.ip + " with attributes(color=" + ipColor + ")" + "\n";
-			output.innerHTML = output.innerHTML + "add vertex " + jsonLine.country.replace(/\s+/g, '') + " with attributes(color=" + countryColor + ")"+ "\n";
+			output.innerHTML = output.innerHTML + "add vertex ip" + jsonLine.src + " with attributes(color=" + srcIpColor + ")" + "\n";
+			output.innerHTML = output.innerHTML + "add vertex ip" + jsonLine.dest + " with attributes(color=" + destIpColor + ")" + "\n";
+			output.innerHTML = output.innerHTML + "add vertex " + jsonLine.src_port + " with attributes(color=" + srcPortColor + ")"+ "\n";
+			output.innerHTML = output.innerHTML + "add vertex " + jsonLine.dest_port + " with attributes(color=" + destPortColor + ")"+ "\n";
 		}
 	}
 
 	for (var i = 0; i < line.length; i++) {
 		jsonLine = JSON.parse(line[i]);
-		if(typeof jsonLine.ip == 'undefined' || typeof jsonLine.country == 'undefined'){
-			output.innerHTML = output.innerHTML + "ip or country is undefined in: " + JSON.stringify(jsonLine) + "\n";
+		if(typeof jsonLine.src == 'undefined' ||
+		   typeof jsonLine.src_port == 'undefined' ||
+		   typeof jsonLine.dest == 'undefined' ||
+		   typeof jsonLine.dest_port == 'undefined'){
+			output.innerHTML = output.innerHTML + "One or more of src, src_port, dest, dest_port are undefined in: " + JSON.stringify(jsonLine) + "\n";
 		} else {
-		output.innerHTML = output.innerHTML + "add edge ip" + jsonLine.ip + " - " + jsonLine.country.replace(/\s+/g, '') + "\n";
+			output.innerHTML = output.innerHTML + "add edge ip" + jsonLine.src + " - ip" + jsonLine.dest + "\n";
+			output.innerHTML = output.innerHTML + "add edge ip" + jsonLine.src + " - " + jsonLine.src_port + "\n";
+			output.innerHTML = output.innerHTML + "add edge ip" + jsonLine.dest + " - " + jsonLine.dest_port + "\n";
 		}
 	}
 }
