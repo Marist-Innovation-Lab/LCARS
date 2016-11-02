df5b65e4df82e7e1d8d8ebc7b63527bdc49cd5eb
Liam Harwood <liamrharwood@gmail.com>
2016-11-02 11:52:51 -0400

    Clean up dashboard stats code

--- LCARS/runtime/js/dashboard.js
+++ LCARS/runtime/js/dashboard.js
@@ -7,48 +7,47 @@ var updateMilliseconds = 30000;
 
 // Counts number of profiles in the database for display
 function updateProfileCount() {
-	var profileCount = 0;
 
 	$.getJSON(
       lcarsAPI + "profiles",
       function (data, status) {
          if (data[0].profiles__pid) {
-                $.each(data, function(i, item) {
-                    profileCount++;
-                });
-                $("#profile-count").html(profileCount);
+         	var count = data.length || 0;
+            $("#profile-count").html(count);
           }
-      });	
+      });
+    if($("#profile-count").html() === "") {
+    	$("#profile-count").html(0);
+    }		
 }
 
 // Counts number of response recipes in the database for display
 function updateResponseCount() {
-	var responseCount = 0;
-
+	
 	$.getJSON(
       lcarsAPI + "responserecipes",
       function (data, status) {
          if (data[0].responserecipes__rrid) {
-                $.each(data, function(i, item) {
-                    responseCount++;
-                });
-                $("#response-count").html(responseCount);
+         	var count = data.length || 0;
+            $("#response-count").html(count);
           }
-      });	
+      });
+    if($("#response-count").html() === "") {
+    	$("#response-count").html(0);
+    }		
 }
 
 // Count number of rules on RFW on .84
 function updateFirewallCount() {
-	var firewallCount = 0;
-
 	$.getJSON(
       rfwAPI + "list",
       function (data, status) {
-        $.each(data, function(i, item) {
-            firewallCount++;
-        });
-        $("#firewall-count").html(firewallCount);
-      });	
+      	var count = data.length || 0;
+        $("#firewall-count").html(count);
+      });
+    if($("#firewall-count").html() === "") {
+    	$("#firewall-count").html(0);
+    }	
 }
 
 // Update stats on page load
