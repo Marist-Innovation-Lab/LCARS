001c4497424657a9af50e6a3199b2bd002eb7bbf
Mariah Molenaer <mariah.molenaer1@marist.edu>
2016-11-05 12:54:34 -0400

    Made a script to get most recent attack time for all honeypots

--- LCARS/scripts/getLogs.sh
+++ LCARS/scripts/getLogs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+#
+# This script gets the current honeypot log from longtail, and stores it in separate files based on honeypot hostname
+#
 
+# Download and decompress log file from longtail
 curl -sO http://longtail.it.marist.edu/honey/current-raw-data.gz
 gunzip -f current-raw-data.gz
 
+# Directory to store log files
 log_dir="/var/www/html/lcars/runtime/logs"
 
-cp -f current-raw-data $log_dir/all.log
+#cp -f current-raw-data $log_dir/all.log
+# Find all log entries that match currently known honeypot hostnames, and store them in their own files
 awk '$2 == "AWS" {print}' current-raw-data > $log_dir/aws.log
 awk '$2 == "shepherd" {print}' current-raw-data > $log_dir/shepherd.log
 awk '$2 == "syrtest" {print}' current-raw-data > $log_dir/syrtest.log
@@ -15,4 +21,5 @@ awk '$2 == "erhp2" {print}' current-raw-data > $log_dir/erhp2.log
 awk '$2 == "ecdal2" {print}' current-raw-data > $log_dir/ecdal2.log
 awk '$2 == "cssdn" {print}' current-raw-data > $log_dir/cssdn.log
 
+# Remove original log file
 rm current-raw-data
---
+++ LCARS/scripts/lastAttacked.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# This script gets the date/time all honeypots were most recently attacked
+# Returns an array of JSON objects, hopefully making it easier to handle elsewhere in our code
+#
+
+
+# Directory where all the honeypot logs are stored
+log_dir="/var/www/html/lcars/runtime/logs"
+
+# Begin the JSON output array with an opening square bracket
+output="["
+# Loop through all of the log files, grabbing the hostname and time from the last line of the file (most recent)
+for log_file in $log_dir/*.log; do
+   time=`awk 'END{print $1}' $log_file`
+   # Change time format so its consistent with other time formatting in LCARS
+   formatted_time=`date -d "$time" +'%Y-%m-%d %H:%M:%S'`
+   hostname=`awk 'END{print $2}' $log_file`
+   # Append JSON object to the output array
+   output+='{"hostname":"'$hostname'", "time":"'$formatted_time'"}, '
+done
+
+# Prints final output, replacing the ", " after the final JSON object with a closing square bracket
+echo "${output%, }]"
