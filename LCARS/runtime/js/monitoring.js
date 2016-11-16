e02b25adf8c6b6dada9068fdabf82530f597dfd3
Liam Harwood <liamrharwood@gmail.com>
2016-11-16 13:06:01 -0500

    Add display for system monitoring (iface details)

--- LCARS/runtime/js/monitoring.js
+++ LCARS/runtime/js/monitoring.js
@@ -16,15 +16,33 @@ function populateLcarsLog() {
 	);
 }
 
-function populateOsInfo() {
+function populateOsVersion() {
 	$.getJSON(
 		lcarsAPI + "osversion",
 		function(data, status) {
 			if(status === "success") {
-				var osInfoHTML = "";
-				osInfoHTML += "<b>OS Version:</b> " + data[0].name + " (" + data[0].major + "." + data[0].minor + "." + data[0].patch + ")";
+				var divHTML = "";
+				divHTML += "<b>OS Version:</b> " + data[0].name + " (" + data[0].major + "." + data[0].minor + "." + data[0].patch + ")<br>";
 
-				$("#osquery").html(osInfoHTML);
+				$("#os-version").html(divHTML);
+			}
+		}
+	);
+}
+
+function populateInterfaceDetails() {
+	$.getJSON(
+		lcarsAPI + "interfacedetails",
+		function(data, status) {
+			if(status === "success") {
+				var divHTML = '<div><b>Interface Details</b></div>' + 
+				'<table class="table"><thead><th>Interface</th><th>Outgoing Packets</th><th>Outgoing Bytes</th><th>Incoming Packets</th><th>Incoming Bytes</th></thead>';
+				$.each(data, function(i, item) {
+   					divHTML += '<tr><td>' + item.interface + '</td><td>' + item.opackets + '</td><td>' + item.obytes + '</td><td>' + item.ipackets + '</td><td>' + item.ibytes + '</td></tr>';
+				});
+				divHTML += '</table>';
+
+				$("#interface-details").html(divHTML);
 			}
 		}
 	);
@@ -32,5 +50,6 @@ function populateOsInfo() {
 
 $(document).ready(function () {
 	populateLcarsLog();
-	populateOsInfo();
+	populateOsVersion();
+	populateInterfaceDetails();
 });
