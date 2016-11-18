ca0518d2c71fe21a8c63081c4dd3bcfda07d8646
Liam Harwood <liamrharwood@gmail.com>
2016-11-18 11:50:54 -0500

    Added idea for System monitoring chart

--- LCARS/runtime/js/monitoring.js
+++ LCARS/runtime/js/monitoring.js
@@ -35,14 +35,33 @@ function populateInterfaceDetails() {
 		lcarsAPI + "interfacedetails",
 		function(data, status) {
 			if(status === "success") {
-				var divHTML = '<div><b>Interface Details</b></div>' + 
-				'<table class="table"><thead><th>Interface</th><th>Outgoing Packets</th><th>Outgoing Bytes</th><th>Incoming Packets</th><th>Incoming Bytes</th></thead>';
+				var divHTML = '<b>Interface Details</b>';
+				divHTML += '<table class="table" style="width: 50%;"><thead><th>Outgoing Packets</th><th>Incoming Packets</th><th>Interfaces</th></thead>';
+				divHTML += '<tr><td><canvas id="outgoing-canvas" height="140" width="140" style="margin: 15px 10px 10px 0"></canvas></td>';
+				divHTML += '<td><canvas id="incoming-canvas" height="140" width="140" style="margin: 15px 10px 10px 0"></canvas></td>';
+				divHTML += '<td><ul style="list-style-type:none;">';
+
+				var interfaceLabels = [];
+				var outgoingData = [];
+				var incomingData = [];
+				var colors = [];
+				var hoverColors = [];
 				$.each(data, function(i, item) {
-   					divHTML += '<tr><td>' + item.interface + '</td><td>' + item.opackets + '</td><td>' + item.obytes + '</td><td>' + item.ipackets + '</td><td>' + item.ibytes + '</td></tr>';
+					colors.push(getRandomColor());
+					hoverColors.push('#EEEEEE');
+					divHTML += '<li><i class="fa fa-square" style="color:' + colors[i] + '"></i> ' + item.interface + '</li>';
+   					interfaceLabels.push(item.interface);
+   					outgoingData.push(item.opackets);
+   					incomingData.push(item.ipackets);
 				});
-				divHTML += '</table>';
+				divHTML += '</ul></td></tr></table>';
 
 				$("#interface-details").html(divHTML);
+
+				createInterfaceChart(interfaceLabels, outgoingData, colors, hoverColors, "outgoing-canvas");
+				createInterfaceChart(interfaceLabels, incomingData, colors, hoverColors, "incoming-canvas");
+
+				
 			}
 		}
 	);
@@ -63,9 +82,40 @@ function populateIptablesInfo() {
 	);
 }
 
+function createInterfaceChart(labels, data, colors, hoverColors, canvasid) {
+	var options = {
+      legend: false,
+      responsive: false
+    };
+
+    new Chart(document.getElementById(canvasid), {
+      type: 'pie',
+      tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+      data: {
+        labels: labels,
+        datasets: [{
+          data: data,
+          backgroundColor: colors,
+          hoverBackgroundColor: hoverColors
+        }]
+      },
+      options: options
+    });
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 $(document).ready(function () {
 	populateLcarsLog();
 	populateOsVersion();
 	populateInterfaceDetails();
 	populateIptablesInfo();
+	createInterfaceChart();
 });
