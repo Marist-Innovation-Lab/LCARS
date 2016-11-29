91e9ac43a41bc2ef63faf1653c70dcdb65445711
Mariah Molenaer <mariah.molenaer1@marist.edu>
2016-11-29 12:12:08 -0500

    Improved script that gets log data

--- LCARS/scripts/getLogs.sh
+++ LCARS/scripts/getLogs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script gets the current honeypot log from longtail, and stores it in separate files based on honeypot hostname
+# This script gets the current active honeypot log from longtail, and stores it in separate files based on honeypot hostname
 #
 
 # Download and decompress log file from longtail
@@ -10,16 +10,18 @@ gunzip -f current-raw-data.gz
 # Directory to store log files
 log_dir="/var/www/html/lcars/runtime/logs/longtail"
 
+# Clear current contents of log directory so that log data from inactive honeypots doesn't linger
+rm -rf $log_dir/*
+
 cp -f current-raw-data $log_dir
-# Find all log entries that match currently known honeypot hostnames, and store them in their own files
-awk '$2 == "AWS" {print}' current-raw-data > $log_dir/aws.log
-awk '$2 == "shepherd" {print}' current-raw-data > $log_dir/shepherd.log
-awk '$2 == "syrtest" {print}' current-raw-data > $log_dir/syrtest.log
-awk '$2 == "edu_c" {print}' current-raw-data > $log_dir/edu_c.log
-awk '$2 == "erhp" {print}' current-raw-data > $log_dir/erhp.log
-awk '$2 == "erhp2" {print}' current-raw-data > $log_dir/erhp2.log
-awk '$2 == "ecdal2" {print}' current-raw-data > $log_dir/ecdal2.log
-awk '$2 == "cssdn" {print}' current-raw-data > $log_dir/cssdn.log
+
+# Find all active honeypot hostnames from current-raw-data log and store them in an array 
+honeypots=($(awk '!seen[$2] {print $2} {++seen[$2]}' current-raw-data))
+# Find all log entries that match active honeypot hostnames, and store them in their own files
+for i in "${honeypots[@]}"
+do
+   awk -v i="$i" '$2 == i {print}' current-raw-data > $log_dir/${i,,}.log  #${i,,} changes text to lowercase, so that AWS is saved as aws.log, not AWS.log
+done
 
 # Run python script that converts all the new logs to json
 python /var/www/html/lcars/scripts/Messages-to-Json.py
