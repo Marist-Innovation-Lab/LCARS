73515d81bbc7543849424b2f48c8775bc3c35790
Graham Burek <graham.burek1@gmail.com>
2017-03-06 15:36:53 -0500

    Adds model display in futurepage.html

--- LCARS/runtime/js/ML.js
+++ LCARS/runtime/js/ML.js
@@ -1,15 +1,30 @@
+// ML.js - javascript functions for ML dash in LCARS 
+
 // Populate data on page load
 $(document).ready(function() {
 	populateModels();
 });
 
 function populateModels(){
-  $.getJSON(path, function(data, status){
-  	
+  $.getJSON(_lcarsAPI + "models", function(data, status){
+  		if (status === "success") {
+  			$("#models").empty();
+
+  			$.each(data, function(i,item) {
+  				var name = data[i].model_name;
+
+  				$("#models").append('<tr><th scope="row">' + (i+1) + '</th>'
+  					+ '<td>' + name + '</td>'
+  					+ '<td>' + 'Put description here' + '</td>'
+  					+ '<td>' + '<button type="button" class="btn btn-default btn-xs"><span title="Load Model" class="glyphicon glyphicon-download-alt"></span></button></td>'
+  					+ '</tr>'
+  					);
+  			});
+  		}
   });
 }
 
-function predict(){
+// function predict(){
 
-}
+// }
 
