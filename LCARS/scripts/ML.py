1812ac79c4bcb07e3974f1ca3ff419564283fe46
Graham Burek <graham.burek1@gmail.com>
2017-03-24 12:02:04 -0400

    Modifies ML.py to use configuration files (accessible via LCARS dash.)

--- LCARS/scripts/ML.py
+++ LCARS/scripts/ML.py
@@ -1,32 +1,106 @@
+'''
+Given a model and test matrix to use, this script predicts what class the test
+matrix is classified as.
+
+Only supports binary class models.
+'''
 import numpy
 import os
 import sys
 import json
+import ConfigParser
 from io import StringIO
 from keras.models import model_from_yaml
 
-messages = []
+messages = [] # List of responses to send back to client
+config = ConfigParser.ConfigParser()
 
-messages.append({"log":("Ran " + sys.argv[0])})
+def main():
+  cfg = config.read('ML.ini')
+  if len(cfg) == 0:
+    messages.append({"log":("No configuration file found. Creating basic configuration file" +
+        " ML.ini.")})
 
-yaml_file = open("/var/www/html/lcars/models/" + sys.argv[1],'r')
-loaded_model_yaml = yaml_file.read()
-yaml_file.close()
-loaded_model = model_from_yaml(loaded_model_yaml)
-messages.append({"log":"Loaded model: " + sys.argv[1]})
-# load weights into new model
-loaded_model.load_weights("/var/www/html/lcars/models/" + sys.argv[2])
-messages.append({"log":("Loaded corresponding weights: " + sys.argv[2])})
+    cfgfile = open("ML.ini",'w')
 
-test_matrix = unicode(open("/var/www/html/lcars/tests/" + sys.argv[3],'r').read())
+    config.add_section('Defaults')
+    config.set('Defaults','shape_x',1)
+    config.set('Defaults','shape_y', 5625)
+    config.set('Defaults','class_0', 0)
+    config.set('Defaults','class_1', 1)
+    config.add_section('ModelOptions')
+    config.set('ModelOptions','model_dir','/var/www/html/lcars/models/')
+    config.add_section('TestOptions')
+    config.set('TestOptions','test_dir','/var/www/html/lcars/tests/')
+    config.write(cfgfile)
+    cfgfile.close()
 
-matrix = numpy.loadtxt(StringIO(test_matrix),delimiter=',').reshape(1,5625)
-prediction = loaded_model.predict(matrix)
-result = numpy.argmax(prediction)
-    
-if result == 0:
-    messages.append({"message":("random " + "(" + str(prediction[0][result]) + "% confidence)")})
-elif result == 1:
-    messages.append({"message":("fully connected " + "(" + str(prediction[0][result]) + "% confidence) ")})
+    config.read('ML.ini')
 
-print(json.dumps(messages))
+  try:
+      MODEL_DIR = ConfigSectionMap("ModelOptions")['model_dir']
+      TEST_DIR  = ConfigSectionMap("TestOptions")['test_dir']
+      SHAPE_X   = ConfigSectionMap(sys.argv[1])['shape_x']
+      SHAPE_Y   = ConfigSectionMap(sys.argv[1])['shape_y']
+      CLASS_0   = ConfigSectionMap(sys.argv[1])['class_0']
+      CLASS_1   = ConfigSectionMap(sys.argv[1])['class_1']
+  except (KeyError,ConfigParser.NoSectionError):
+      messages.append({"log":("Could not find all configuration options for " + sys.argv[1] +
+          ". Using defaults.")})
+      MODEL_DIR = ConfigSectionMap("ModelOptions")['model_dir']
+      TEST_DIR  = ConfigSectionMap("TestOptions")['test_dir']
+      SHAPE_X   = ConfigSectionMap("Defaults")['shape_x']
+      SHAPE_Y   = ConfigSectionMap("Defaults")['shape_y']
+      CLASS_0   = ConfigSectionMap("Defaults")['class_0']
+      CLASS_1   = ConfigSectionMap("Defaults")['class_1']
+
+  # messages.append({"log":("Started command: " +
+  #     sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3])})
+
+  yaml_file = open(MODEL_DIR + sys.argv[1],'r')
+  loaded_model_yaml = yaml_file.read()
+  yaml_file.close()
+
+  loaded_model = model_from_yaml(loaded_model_yaml)
+  messages.append({"log":"Loaded model: " + sys.argv[1]})
+  # load weights into new model
+  loaded_model.load_weights(MODEL_DIR + sys.argv[2]) 
+  messages.append({"log":("Loaded corresponding weights: " + sys.argv[2])})
+  # StringIO needs a unicode string
+  test_matrix = unicode(open(TEST_DIR + sys.argv[3],'r').read()) 
+  matrix = numpy.loadtxt(StringIO(test_matrix),delimiter=',').reshape(int(SHAPE_X),
+                                                                      int(SHAPE_Y)
+                                                                     )
+  prediction = loaded_model.predict(matrix)
+
+  result = numpy.argmax(prediction) # Gets index of most likely outcome
+
+  if result == 0:
+      messages.append({"message":(CLASS_0 + " (" + str(prediction[0][result]) +
+          "% confidence)")})
+  elif result == 1:
+      messages.append({"message":(CLASS_1 + " (" + str(prediction[0][result]) + 
+          "% confidence) ")})
+
+  print(json.dumps(messages))
+
+'''
+Looks up values in an .ini file.
+
+See https://wiki.python.org/moin/ConfigParserExamples
+'''
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+if __name__ == '__main__':
+    main()
