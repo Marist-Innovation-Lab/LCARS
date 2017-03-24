c07a65d831f79edd59a2dc70d3055e69b2f14f0a
Graham Burek <graham.burek1@gmail.com>
2017-03-24 12:12:39 -0400

    Adds radio buttons to select tests and models. Adds function to load tests from API.

--- LCARS/runtime/js/ML.js
+++ LCARS/runtime/js/ML.js
@@ -3,55 +3,77 @@
 currentModel = "";
 // Populate data on page load
 $(document).ready(function() {
-	populateModels();
-	registerButtons();
+  populateModels();
+  populateTests();
+  registerButtons();
 });
 
+// Fills list of models using call to API
 function populateModels(){
   $.getJSON(_lcarsAPI + "models", function(data, status){
-  		if (status === "success") {
-  			$("#models").empty();
+      if (status === "success") {
+        $("#models").empty();
 
-  			$.each(data, function(i,item) {
-  				var name = data[i].model_name;
+        $.each(data, function(i,item) {
+          var name = data[i].model_name;
 
-  				$("#models").append('<tr><th scope="row">' + (i+1) + '</th>'
-  					+ '<td>' + name + '</td>'
-  					+ '<td>' + 'Put description here' + '</td>'
-  					+ '<td>' + '<button type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#model-modal"><span title="Make prediction" class="glyphicon glyphicon-question-sign"></span></button></td>'
-  					+ '</tr>'
-  					);
-  			});
-  		}
+          $("#models").append('<tr><th scope="row">' + (i+1) + '</th>'
+            + '<td>' + name + '</td>'
+            + '<td>' + 'Put description here' + '</td>'
+            + '<td>' + '<span class="radio-inline"><input name="model_radios" type="radio" value=' + name +'></span></td>'  
+            + '</tr>'
+            );
+        });
+      }
   });
 }
 
-// Predicts an outcome, given a model and a matrix to predict for
+// Fills list of test inputs using call to API
+function populateTests(){
+  $.getJSON(_lcarsAPI + "tests", function(data, status){
+      if (status === "success") {
+        $("#tests").empty();
+
+        $.each(data, function(i,item) {
+          var name = data[i].test_name;
+
+          $("#tests").append('<tr><th scope="row">' + (i+1) + '</th>'
+            + '<td>' + name + '</td>'
+            + '<td>' + 'Put description here' + '</td>'
+            + '<td>' + '<span class="radio-inline"><input name="test_radios" type="radio" value=' + name +'></span></td>'
+            + '</tr>'
+            );
+        });
+      }
+  });
+}
+
+// Predicts an outcome, given a model and a test case to predict for
 function predict(model_name, matrix){
-	dataObject = {'model_name':model_name, 'model_weights': model_name.replace("yaml","h5"), 'matrix':matrix};
-	$.ajax({
+  dataObject = {'model_name':model_name, 'model_weights': model_name.replace("yaml","h5"), 'matrix':matrix};
+  $.ajax({
             url: _lcarsAPI + "predict",
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(dataObject),
             success: function(data) { console.log(data); }
-    });
+  });
 }
 
 // Adds onclick events to buttons
 function registerButtons(){
-	registerPredictButton();
-	registerTableButtons();
+  registerPredictButton();
+  registerTableButtons();
 }
 
 function registerTableButtons(){
-	$("#models").on("click", "td button", function() {
-		currentModel = $(this).closest("tr").find("td:nth-child(2)").text();
-	});
+  $("#models").on("click", "td button", function() {
+    currentModel = $(this).closest("tr").find("td:nth-child(2)").text();
+  });
 }
 
 function registerPredictButton(){
-	$('#predict-button').click(function() {
-		predict(currentModel, $('#predict-data').val());
-	});
+  $('#predict-button').click(function() {
+    predict(currentModel, $('#predict-data').val());
+  });
 }
